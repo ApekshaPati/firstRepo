055f6d5c3bdf3f56a6e44c96fe1b8ec4e284d686
Dell <Dell@Dell-PC>
2023-01-19 12:01:55 +0530

    1st commit done  Teast class added

---
+++ TestRepo/src/ArrayIndex.java
@@ -0,0 +1,36 @@
+
+public class ArrayIndex {
+	
+	
+	public static void main(String[] args) {
+		
+		int [] d = {23,34,45,56,67,78,12};
+		
+		for(int i=0; i<d.length; i++) {
+			System.out.print(d[i]+ " ");
+		}
+		System.out.println();
+		
+		//to reverse index //  swapping
+		
+		for(int i=0; i<(d.length)/2; i++) {
+		int x = d[i];
+		d[i] = d[((d.length-1)-i)];
+		d[((d.length-1)-i)] = x;
+		
+		
+		}
+		System.out.print(d[1]);
+		System.out.println();
+		
+		// to print value of d
+		for(int i=0; i<d.length; i++) {
+			
+			
+			System.out.print(d[i]+" ");
+			
+		}
+		
+	}
+
+}
